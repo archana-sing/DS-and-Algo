3f35be68078c27d0db918cb9dc8bfb1a55aa81d0
archana singh <archanasingh1643@gmail.com>
2021-05-15 20:47:24 +0530

    added two sum problem

---
+++ Two_sum_problem.js
@@ -0,0 +1,16 @@
+let arr = [ 3, 5, 9, 2, 8, 10, 11 ];
+let sum = 17;
+let i = 0; 
+let j = arr.length -1;
+while(i < j){
+    if(arr[i] + arr[j] === sum){
+        console.log(arr[i] , arr[j]);
+        break;
+    }
+    else if(arr[i] + arr[j] < sum){
+        i++
+    }
+    else{
+        j--
+    }
+}
